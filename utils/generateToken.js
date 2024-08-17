64f4dfeb605765db132c99791240ccfa279b5b6c
Yaser Siddiquee <yaser.siddiquee@gmail.com>
2024-08-18 03:35:55 +0530

    samesite set to none

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -10,7 +10,7 @@ const generateTokenAndSetCookie = (userId, res) => {
     res.cookie("jwt", token, {
         maxAge: 1*24*60*60*1000,
         httpOnly: true,
-        sameSite: "strict", 
+        sameSite: "None", 
         secure: true 
     })
 
